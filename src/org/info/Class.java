a6dd1b68ee6e799e6be2d4de0233b07b6d2cdfe3
pushreji <pushreji2@gmail.com>
2021-09-18 11:52:56 +0530

    Pushpa tast

---
+++ src/org/info/Class.java
@@ -0,0 +1,16 @@
+package org.info;
+
+public class Class {
+private void push() {
+		
+		System.out.println("a");
+	}
+	private void push1() {
+		
+		System.out.println("b");
+	}
+	private void push3() {
+		System.out.println("c");
+	}
+
+}
---
+++ src/org/info/Class1.java
@@ -0,0 +1,16 @@
+package org.info;
+
+public class Class1 {
+	private void neeven() {
+		
+		System.out.println("navis");
+	}
+	private void neeven2() {
+		
+		System.out.println("navis1");
+	}
+	private void neeven3() {
+		System.out.println("navis2");
+	}
+
+}
